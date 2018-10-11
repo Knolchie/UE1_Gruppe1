8626d5f660dea95ed35e3d49b87a9c69913614b1
Haas Matteo Johannes <1810653932@stud.fh-kufstein.ac.at>
2018-10-11 15:22:26 +0200

    first commit

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main {
     public static void main(String[] args) {
 
         String vorname = "Matteo";
+        int alter = 19;
 
-        System.out.println("Hello " + vorname + "!");
+        System.out.println("Hello " + vorname + "! du bist " + alter + " Jahre alt. ");
     }
 }
